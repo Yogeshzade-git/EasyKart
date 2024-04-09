468275247b77bb52d7cec29f5a0d0c541d2fe60a
Yogeshzade-git <ymzade2019@gmail.com>
2024-04-09 20:18:31 +0530

    initial

---
+++ EasyCart/src/main/java/com/yogeshprojects/EasyCart/EasyCartApplication.java
@@ -0,0 +1,13 @@
+package com.yogeshprojects.EasyCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCartApplication.class, args);
+	}
+
+}
---
+++ EasyCart/src/test/java/com/yogeshprojects/EasyCart/EasyCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogeshprojects.EasyCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
