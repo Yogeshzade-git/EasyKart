1c03eb1f41ff1d487125fe1a6789c564d11276c2
Yogeshzade-git <ymzade2019@gmail.com>
2024-04-10 23:46:12 +0530

    .

--- EasyCartWeb/src/app/app.module.ts
+++ EasyCartWeb/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,10 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DemoAngularMaterialModule
+    DemoAngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration(),
--- EasyCartWeb/src/app/signup/signup.component.ts
+++ EasyCartWeb/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  signupForm!: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder,
+    private snackbar: MatSnackBar,
+    private authService : AuthService,
+    private router : Router,){
+
+    }
+
+    ngOnInit(): void{
+      this.signupForm = this.fb.group({
+        name:[null, [Validators.required]],
+        email:[null,[Validators.required]],
+        password:[null,[Validators.required]],
+        confirmPassword:[null, [Validators.required]],
+      })
+    }
+
+    togglePasswordVisibility(){
+      this.hidePassword = !this.hidePassword;
+    }
+
+    onSubmit(): void{
+      const password = this.signupForm.get('password')?.value;
+      const confirmPassword = this.signupForm.get('confirmPassword')?.value;
+
+      if(password !== confirmPassword){
+        this.snackbar.open('Password does not match.', 'close', {duration: 5000, panelClass: 'error-snackbar'});
+        return;
+      }
+
+      this.authService.register(this.signupForm.value).subscribe(
+        (response) =>{
+            this.snackbar.open('signup successful!!', 'close', {duration:5000});
+            this.router.navigateByUrl("/login");
+        },
+        (error) => {
+            this.snackbar.open('signup failed. please try again.','close', {duration:5000, panelClass:'error-snackbar'});
+        }
+      )
+    }
 
 }
